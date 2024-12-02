4d5106303ef6d67efa0887f86c57bba2ca9ecf65
zqzneptune <5545472+zqzneptune@users.noreply.github.com>
2024-12-02 09:17:17 -0400

    Check Salmon exists

--- buildGencodeSalmonIndex.sh
+++ buildGencodeSalmonIndex.sh
@@ -50,115 +50,121 @@ if [ ! -f "${SALMON}" ]; then
     wget -c https://github.com/COMBINE-lab/salmon/releases/download/v${VERSALMON}/salmon-${VERSALMON}_linux_x86_64.tar.gz
     tar xzvf salmon-${VERSALMON}_linux_x86_64.tar.gz
 fi
-mv salmon-latest_linux_x86_64 salmon-${VERSALMON}_linux_x86_64
+
+if [ -d "salmon-latest_linux_x86_64"]; then
+    mv salmon-latest_linux_x86_64 salmon-${VERSALMON}_linux_x86_64
 
 
-################### Gencode Human ##############
-DIRINDEXH=index/${VERSALMON}/human_gencode${VERGENCODEH}
+    ################### Gencode Human ##############
+    DIRINDEXH=index/${VERSALMON}/human_gencode${VERGENCODEH}
 
 
-FNTXFAH=gencode.v${VERGENCODEH}.transcripts.fa
-FNTXGTFH=gencode.v${VERGENCODEH}.annotation.gtf
+    FNTXFAH=gencode.v${VERGENCODEH}.transcripts.fa
+    FNTXGTFH=gencode.v${VERGENCODEH}.annotation.gtf
 
-mkdir -p ${DIRINDEXH}
-mkdir -p ${DIRINDEXH}\_k15
+    mkdir -p ${DIRINDEXH}
+    mkdir -p ${DIRINDEXH}\_k15
 
 
-if [ ${VERGENCODEH} -ge 20 ]; then
-    FNGNEOME=GRCh38.primary_assembly.genome.fa
+    if [ ${VERGENCODEH} -ge 20 ]; then
+        FNGNEOME=GRCh38.primary_assembly.genome.fa
+    else
+        echo "Gencode too old"
+        exit 1
+    fi
+
+    if [ -f "${FNGNEOME}" ]; then
+        echo "${FNGNEOME} exists."
+    else 
+        echo "${FNGNEOME} does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERGENCODEH}/${FNGNEOME}.gz
+        gunzip ${FNGNEOME}.gz
+    fi
+
+    if [ -f "${FNTXFAH}" ]; then
+        echo "${FNTXFAH} exists."
+    else
+        echo "${FNTXFAH}  does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERGENCODEH}/${FNTXFAH}.gz
+        gunzip ${FNTXFAH}.gz
+    fi
+
+    if [ -f "${FNTXFAH}" ]; then
+        cat ${FNTXFAH}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXH}/transcripts.txt
+        gzip ${DIRINDEXH}/transcripts.txt
+        cat ${FNTXFAH}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXH}\_k15/transcripts.txt
+        gzip ${DIRINDEXH}\_k15/transcripts.txt
+    fi
+
+    if [ -f "${FNTXGTFH}" ]; then
+        echo "${FNTXGTFH} exists."
+    else
+        echo "${FNTXGTFH}  does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_$VERGENCODEH/${FNTXGTFH}.gz
+        gunzip ${FNTXGTFH}.gz
+    fi
+
+    bash ${SHDECOY} -j 8 -b $BEDTOOLS -m $MASHMAP -a ${FNTXGTFH} -g ${FNGNEOME} -t ${FNTXFAH} -o $DIRINDEXH
+
+    $SALMON index -t $DIRINDEXH/gentrome.fa -d $DIRINDEXH/decoys.txt -i $DIRINDEXH --gencode
+
+    $SALMON index -t $DIRINDEXH/gentrome.fa -d $DIRINDEXH/decoys.txt -i $DIRINDEXH\_k15 -k 15 --gencode
+
+
+    ###################### Mouse Gencode ##################
+    DIRINDEXM=index/${VERSALMON}/mouse_gencodeM${VERGENCODEM}
+    mkdir -p ${DIRINDEXM}
+    mkdir -p ${DIRINDEXM}\_k15
+
+    FNTXFAM=gencode.vM${VERGENCODEM}.transcripts.fa
+    FNTXGTFM=gencode.vM${VERGENCODEM}.annotation.gtf
+
+    if [ ${VERGENCODEM} -ge 26 ]; then
+        FNGNEOMEM=GRCm39.primary_assembly.genome.fa
+    elif [ ${VERGENCODEM} -ge 2 ]; then
+        FNGNEOMEM=GRCm38.primary_assembly.genome.fa
+    else
+        echo "Gencode too old"
+        exit 1
+    fi
+
+    if [ -f "${FNGNEOMEM}" ]; then
+        echo "${FNGNEOMEM} exists."
+    else 
+        echo "${FNGNEOMEM} does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M${VERGENCODEM}/${FNGNEOMEM}.gz
+        gunzip ${FNGNEOMEM}.gz
+    fi
+
+    if [ -f "${FNTXFAM}" ]; then
+        echo "${FNTXFAM} exists."
+    else
+        echo "${FNTXFAM}  does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M${VERGENCODEM}/${FNTXFAM}.gz
+        gunzip ${FNTXFAM}.gz
+    fi
+
+    if [ -f "${FNTXFAM}" ]; then
+        cat ${FNTXFAM}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXM}/transcripts.txt
+        gzip ${DIRINDEXM}/transcripts.txt
+        cat ${FNTXFAM}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXM}\_k15/transcripts.txt
+        gzip ${DIRINDEXM}\_k15/transcripts.txt
+    fi
+
+    if [ -f "${FNTXGTFM}" ]; then
+        echo "${FNTXGTFM} exists."
+    else
+        echo "${FNTXGTFM}  does not exist ... start downloading ..."
+        wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M$VERGENCODEM/${FNTXGTFM}.gz
+        gunzip ${FNTXGTFM}.gz
+    fi
+
+    sh ${SHDECOY} -j 8 -b $BEDTOOLS -m $MASHMAP -a ${FNTXGTFM} -g ${FNGNEOMEM} -t ${FNTXFAM} -o $DIRINDEXM
+
+    $SALMON index -t $DIRINDEXM/gentrome.fa -d $DIRINDEXM/decoys.txt -i $DIRINDEXM --gencode
+
+    $SALMON index -t $DIRINDEXM/gentrome.fa -d $DIRINDEXM/decoys.txt -i $DIRINDEXM\_k15 -k 15 --gencode
+
 else
-    echo "Gencode too old"
-    exit 1
+    echo "Salmon does not exist!"
 fi
-
-if [ -f "${FNGNEOME}" ]; then
-    echo "${FNGNEOME} exists."
-else 
-    echo "${FNGNEOME} does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERGENCODEH}/${FNGNEOME}.gz
-    gunzip ${FNGNEOME}.gz
-fi
-
-if [ -f "${FNTXFAH}" ]; then
-    echo "${FNTXFAH} exists."
-else
-    echo "${FNTXFAH}  does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${VERGENCODEH}/${FNTXFAH}.gz
-    gunzip ${FNTXFAH}.gz
-fi
-
-if [ -f "${FNTXFAH}" ]; then
-    cat ${FNTXFAH}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXH}/transcripts.txt
-    gzip ${DIRINDEXH}/transcripts.txt
-    cat ${FNTXFAH}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXH}\_k15/transcripts.txt
-    gzip ${DIRINDEXH}\_k15/transcripts.txt
-fi
-
-if [ -f "${FNTXGTFH}" ]; then
-    echo "${FNTXGTFH} exists."
-else
-    echo "${FNTXGTFH}  does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_$VERGENCODEH/${FNTXGTFH}.gz
-    gunzip ${FNTXGTFH}.gz
-fi
-
-bash ${SHDECOY} -j 8 -b $BEDTOOLS -m $MASHMAP -a ${FNTXGTFH} -g ${FNGNEOME} -t ${FNTXFAH} -o $DIRINDEXH
-
-$SALMON index -t $DIRINDEXH/gentrome.fa -d $DIRINDEXH/decoys.txt -i $DIRINDEXH --gencode
-
-$SALMON index -t $DIRINDEXH/gentrome.fa -d $DIRINDEXH/decoys.txt -i $DIRINDEXH\_k15 -k 15 --gencode
-
-
-###################### Mouse Gencode ##################
-DIRINDEXM=index/${VERSALMON}/mouse_gencodeM${VERGENCODEM}
-mkdir -p ${DIRINDEXM}
-mkdir -p ${DIRINDEXM}\_k15
-
-FNTXFAM=gencode.vM${VERGENCODEM}.transcripts.fa
-FNTXGTFM=gencode.vM${VERGENCODEM}.annotation.gtf
-
-if [ ${VERGENCODEM} -ge 26 ]; then
-    FNGNEOMEM=GRCm39.primary_assembly.genome.fa
-elif [ ${VERGENCODEM} -ge 2 ]; then
-    FNGNEOMEM=GRCm38.primary_assembly.genome.fa
-else
-    echo "Gencode too old"
-    exit 1
-fi
-
-if [ -f "${FNGNEOMEM}" ]; then
-    echo "${FNGNEOMEM} exists."
-else 
-    echo "${FNGNEOMEM} does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M${VERGENCODEM}/${FNGNEOMEM}.gz
-    gunzip ${FNGNEOMEM}.gz
-fi
-
-if [ -f "${FNTXFAM}" ]; then
-    echo "${FNTXFAM} exists."
-else
-    echo "${FNTXFAM}  does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M${VERGENCODEM}/${FNTXFAM}.gz
-    gunzip ${FNTXFAM}.gz
-fi
-
-if [ -f "${FNTXFAM}" ]; then
-    cat ${FNTXFAM}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXM}/transcripts.txt
-    gzip ${DIRINDEXM}/transcripts.txt
-    cat ${FNTXFAM}| grep "^>"|sed 's|[<>,]||g' > ${DIRINDEXM}\_k15/transcripts.txt
-    gzip ${DIRINDEXM}\_k15/transcripts.txt
-fi
-
-if [ -f "${FNTXGTFM}" ]; then
-    echo "${FNTXGTFM} exists."
-else
-    echo "${FNTXGTFM}  does not exist ... start downloading ..."
-    wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M$VERGENCODEM/${FNTXGTFM}.gz
-    gunzip ${FNTXGTFM}.gz
-fi
-
-sh ${SHDECOY} -j 8 -b $BEDTOOLS -m $MASHMAP -a ${FNTXGTFM} -g ${FNGNEOMEM} -t ${FNTXFAM} -o $DIRINDEXM
-
-$SALMON index -t $DIRINDEXM/gentrome.fa -d $DIRINDEXM/decoys.txt -i $DIRINDEXM --gencode
-
-$SALMON index -t $DIRINDEXM/gentrome.fa -d $DIRINDEXM/decoys.txt -i $DIRINDEXM\_k15 -k 15 --gencode
